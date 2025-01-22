d5ed831fc120b4af4d6f5d3cf440de6e4eaafffa
Ashish Ghimire <ashishdotenv@gmail.com>
2025-01-22 21:03:30 +0545

    Mobile Navmenu Fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,21 +14,22 @@ const Navbar = () => {
 
     return (
         <nav className="bg-white shadow-md z-10">
-            <div class="flex justify-around items-center py-4">
-                <div class="flex-shrink-0">
-                    <a href="#" class="text-2xl font-extrabold text-blue-600">Badimalika School</a>
+            <div className="flex justify-around items-center py-4">
+                <div className="flex-shrink-0">
+                    <a href="#" className="text-2xl font-extrabold text-blue-600">
+                        Badimalika School
+                    </a>
                 </div>
-
-                <div class="hidden md:flex items-center space-x-6">
-                    <div class="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
+                <div className="hidden md:flex items-center space-x-6">
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M2.003 5.884L10 10.882l7.997-4.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884zM18 8.118l-8 5-8-5V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                         </svg>
                         <span>info@badimalikasecschool.com</span>
                     </div>
-                    <div class="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm12 4H6v2h8V9z" clip-rule="evenodd" />
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm12 4H6v2h8V9z" clipRule="evenodd" />
                         </svg>
                         <span>+977-9758900824</span>
                     </div>
@@ -36,7 +37,6 @@ const Navbar = () => {
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
-                    {/* Main Navigation */}
                     <div className="hidden md:flex space-x-8">
                         <Link className="hover:text-blue-600 font-medium" to="/">
                             Home
@@ -44,29 +44,18 @@ const Navbar = () => {
                         <Link className="hover:text-blue-600 font-medium" to="/about-us">
                             About Us
                         </Link>
-
-                        {/* Academics Dropdown */}
                         <div className="relative group">
                             <button className="flex items-center space-x-1 hover:text-blue-600 font-medium">
                                 <span>Academics</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-4 w-4"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z"
-                                        clipRule="evenodd"
-                                    />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z" clipRule="evenodd" />
                                 </svg>
                             </button>
                             <div className="absolute left-0 mt-2 bg-white text-gray-800 rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity">
-                                <Link className="block px-4 py-2 hover:bg-gray-100" to="/academics/curriculum/">
+                                <Link className="block px-4 py-2 hover:bg-gray-100" to="/academics/curriculum">
                                     Curriculum
                                 </Link>
-                                <Link className="block px-4 py-2 hover:bg-gray-100" to="/academics/faculty/">
+                                <Link className="block px-4 py-2 hover:bg-gray-100" to="/academics/faculty">
                                     Faculty
                                 </Link>
                                 <Link className="block px-4 py-2 hover:bg-gray-100" to="/academics/admission">
@@ -74,22 +63,11 @@ const Navbar = () => {
                                 </Link>
                             </div>
                         </div>
-
-                        {/* Activities Dropdown */}
                         <div className="relative group">
                             <button className="flex items-center space-x-1 hover:text-blue-600 font-medium">
                                 <span>Activities</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-4 w-4"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z"
-                                        clipRule="evenodd"
-                                    />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z" clipRule="evenodd" />
                                 </svg>
                             </button>
                             <div className="absolute left-0 mt-2 bg-white text-gray-800 rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity">
@@ -102,114 +80,71 @@ const Navbar = () => {
                                 <Link className="block px-4 py-2 hover:bg-gray-100" to="/activities/events">
                                     Events
                                 </Link>
-                                
                             </div>
                         </div>
-
                         <Link className="hover:text-blue-600 font-medium" to="/contact">
                             Contact
                         </Link>
                     </div>
-
-                    {/* Mobile Menu Button */}
                     <div className="md:hidden">
                         <button
                             id="mobile-menu-btn"
                             className="text-gray-800 focus:outline-none"
                             onClick={toggleMobileMenu}
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16m-7 6h7"
-                                />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                             </svg>
                         </button>
                     </div>
                 </div>
             </div>
-
-            {/* Mobile Menu */}
             <div id="mobile-menu" className="hidden md:hidden bg-white shadow-lg">
-                <Link className="block px-4 py-2 hover:bg-gray-100" to="/">
+                <Link className="block px-4 py-2 hover:bg-gray-100" to="/" onClick={toggleMobileMenu}>
                     Home
                 </Link>
-                <Link className="block px-4 py-2 hover:bg-gray-100" to="/about-us/about">
+                <Link className="block px-4 py-2 hover:bg-gray-100" to="/about-us" onClick={toggleMobileMenu}>
                     About Us
                 </Link>
                 <div className="relative">
-                    <button
-                        id="mobile-academics-btn"
-                        className="w-full flex justify-between px-4 py-2 hover:bg-gray-100"
-                        onClick={() => toggleDropdown("mobile-academics")}
-                    >
+                    <button id="mobile-academics-btn" className="w-full flex justify-between px-4 py-2 hover:bg-gray-100" onClick={() => toggleDropdown("mobile-academics")}>
                         <span>Academics</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                            />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z" clipRule="evenodd" />
                         </svg>
                     </button>
                     <div id="mobile-academics" className="hidden bg-gray-100">
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/curriculum">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/curriculum" onClick={toggleMobileMenu}>
                             Curriculum
                         </Link>
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/faculty">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/faculty" onClick={toggleMobileMenu}>
                             Faculty
                         </Link>
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/admission">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/academics/admission" onClick={toggleMobileMenu}>
                             Admission
                         </Link>
                     </div>
                 </div>
                 <div className="relative">
-                    <button
-                        id="mobile-activities-btn"
-                        className="w-full flex justify-between px-4 py-2 hover:bg-gray-100"
-                        onClick={() => toggleDropdown("mobile-activities")}
-                    >
+                    <button id="mobile-activities-btn" className="w-full flex justify-between px-4 py-2 hover:bg-gray-100" onClick={() => toggleDropdown("mobile-activities")}>
                         <span>Activities</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                            />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.707a1 1 0 011.414 0z" clipRule="evenodd" />
                         </svg>
                     </button>
                     <div id="mobile-activities" className="hidden bg-gray-100">
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/sports">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/sports" onClick={toggleMobileMenu}>
                             Sports
                         </Link>
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/clubs">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/clubs" onClick={toggleMobileMenu}>
                             Clubs
                         </Link>
-                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/events">
+                        <Link className="block px-4 py-2 hover:bg-gray-200" to="/activities/events" onClick={toggleMobileMenu}>
                             Events
                         </Link>
                     </div>
                 </div>
-                <Link className="block px-4 py-2 hover:bg-gray-100" to="/contact/contact">
+                <Link className="block px-4 py-2 hover:bg-gray-100" to="/contact/contact" onClick={toggleMobileMenu}>
                     Contact
                 </Link>
             </div>
